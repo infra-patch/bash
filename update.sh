51b8de126217ce0bbc94daefc3bdcfd61aef7348
tom-sapletta-com <tom@sapletta.com>
2022-05-29 09:47:12 +0200

    update documentation, apimacro test install

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PROJECT_LIST="projects.txt"
-while IFS= read -r git_repo; do
-  repo=( $git_repo )
-  echo "UPDATE PROJECT: ${repo[1]}/  FROM REPO:  (${repo[0]})"
-  cd ${repo[1]} && git pull
-  cd ..
-done <"$PROJECT_LIST"
-
